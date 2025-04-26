6469f0b1abfa3b4e4b8470bd02301d7a8f9705bd
emreertugrl <emrertugrl@gmail.com>
2025-04-26 20:54:12 +0300

    add new list screen and name

--- src/router/rootNavigation.js
+++ src/router/rootNavigation.js
@@ -1,9 +1,11 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {GETSTARTED, SIGNIN, WATCHLIST} from '../utils/routes';
+import {ADDNEWWATCHLIST, GETSTARTED, SIGNIN, WATCHLIST} from '../utils/routes';
 import WatchList from '../screens/watchList';
 import GetStarted from '../screens/getStarted';
 import SignIn from '../screens/signIn';
 import {ThemeColors} from '../theme/themeColors';
+import {Edit2} from 'iconsax-react-nativejs';
+import AddNewWatchList from '../screens/watchList/addNewWatchList';
 
 const Stack = createNativeStackNavigator();
 
@@ -23,8 +25,21 @@ export default RootNavigation = () => {
         name={GETSTARTED}
         component={GetStarted}
       />
+      <Stack.Screen
+        options={({navigation}) => ({
+          headerRight: () => (
+            <Edit2
+              onPress={() => navigation.navigate(ADDNEWWATCHLIST)}
+              color={ThemeColors.WHITE}
+              size={25}
+            />
+          ),
+        })}
+        name={WATCHLIST}
+        component={WatchList}
+      />
+      <Stack.Screen name={ADDNEWWATCHLIST} component={AddNewWatchList} />
       <Stack.Screen name={SIGNIN} component={SignIn} />
-      <Stack.Screen name={WATCHLIST} component={WatchList} />
     </Stack.Navigator>
   );
 };
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,3 +1,4 @@
 export const GETSTARTED = 'Get Started';
 export const SIGNIN = 'Sign In';
 export const WATCHLIST = 'Watch List';
+export const ADDNEWWATCHLIST = 'Add New Watch List';
