c922cf2ab9a776d5ebd06497546a4cbca0a5a32a
emreertugrl <emrertugrl@gmail.com>
2025-04-26 20:51:57 +0300

    default screen style

---
+++ src/styles/defaultScreenStyle.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+import {ThemeColors} from '../theme/themeColors';
+
+export const defaultScreenStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: ThemeColors.BLACK,
+    padding: 10,
+  },
+});
