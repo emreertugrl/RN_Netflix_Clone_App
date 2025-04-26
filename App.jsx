3c00a8fd1ff0cdbbfce3426746d768ec547ee35b
emreertugrl <emrertugrl@gmail.com>
2025-04-26 16:41:24 +0300

    status bar made transparent

--- App.jsx
+++ App.jsx
@@ -2,10 +2,12 @@ import {NavigationContainer} from '@react-navigation/native';
 import RootNavigation from './src/router/rootNavigation';
 import {Provider} from 'react-redux';
 import {store} from './src/store';
+import {StatusBar} from 'react-native';
 
 export default function App() {
   return (
     <Provider store={store}>
+      <StatusBar backgroundColor="transparent" translucent />
       <NavigationContainer>
         <RootNavigation />
       </NavigationContainer>
