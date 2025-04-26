159e12eb96d59d2aba4889efeb7da9b71a3a8bed
emreertugrl <emrertugrl@gmail.com>
2025-04-26 18:34:18 +0300

    root navigation style defined

--- src/router/rootNavigation.js
+++ src/router/rootNavigation.js
@@ -3,13 +3,26 @@ import {GETSTARTED, SIGNIN, WATCHLIST} from '../utils/routes';
 import WatchList from '../screens/watchList';
 import GetStarted from '../screens/getStarted';
 import SignIn from '../screens/signIn';
+import {ThemeColors} from '../theme/themeColors';
 
 const Stack = createNativeStackNavigator();
 
 export default RootNavigation = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name={GETSTARTED} component={GetStarted} />
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: ThemeColors.WHITE,
+        headerStyle: {
+          backgroundColor: ThemeColors.BLACK,
+        },
+      }}>
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+        name={GETSTARTED}
+        component={GetStarted}
+      />
       <Stack.Screen name={SIGNIN} component={SignIn} />
       <Stack.Screen name={WATCHLIST} component={WatchList} />
     </Stack.Navigator>
