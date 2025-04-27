d176a7a9250c605d0410107890d91447abbe342d
emreertugrl <emrertugrl@gmail.com>
2025-04-27 11:19:10 +0300

    axios http request and axios headers, base url defined,

---
+++ src/service/instance.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import Config from 'react-native-config';
+import {BASE_URL} from './urls';
+
+const apiKey = Config.IMDB_API_KEY;
+const apiToken = Config.IMDB_API_TOKEN;
+
+const Client = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${apiToken}`,
+  },
+  params: {
+    api_key: apiKey,
+    language: 'tr-TR',
+  },
+});
+
+export default Client;
